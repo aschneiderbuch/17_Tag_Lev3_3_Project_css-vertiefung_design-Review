0ec4cc7a8cdc5db0f13dacdb945f6bdfcfe07a0f
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-01-06 00:12:25 +0100

    testingDarkButton

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,8 @@
 window.alert("Bitte um Hilfe !!! \n \n Wieso gibt es einen weißen Rand rechts \n der einen horrizontal Scroll auslöst ;-( \n was muss ich genau im Code ändern damit er weg ist ??? \n \n \n Danke für die Hilfe")
+
+function myFunction() {
+
+    let html = document.querySelector("html");
+
+    html.style.backgroundColor = "rgba(44, 26, 1, 0.766)";
+}
